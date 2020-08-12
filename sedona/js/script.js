987847d117c2a2b3663cea686152311b0aeaaa25
juggler39 <juggler39@yandex.ru>
2020-08-12 17:50:20 +0200

    replace Google map with Yandex map

--- sedona/js/script.js
+++ sedona/js/script.js
@@ -1,15 +1,1 @@
-var link = document.querySelector(".modal-content-btn");
-		var popup = document.querySelector(".modal-content");
-
-		link.addEventListener("click", function (event) {
-			event.preventDefault();
-			popup.classList.toggle("modal-content-show");
-		});
-
-		window.addEventListener("keydown", function (event) {
-			if (event.keyCode === 27) {
-				if (popup.classList.contains("modal-content-show")) {
-					popup.classList.remove("modal-content-show");
-				}
-			}
-		});
+"use strict";var button=document.querySelector(".search__button"),form=document.querySelector(".form");button&&(form.classList.remove("form--no-js"),button.addEventListener("click",function(t){t.preventDefault(),form.classList.toggle("form--close")}));
